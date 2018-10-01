c1b682b57c47c9e285091b24c3a4e83f7626c34b
DRpandaMD <michael.zarate.ce@gmail.com>
2018-10-01 14:09:36 -0500

    updated nodejs script because AWS docs was depreciated, thanks stack overflow

--- aws_nodejs_bootstrap.sh
+++ aws_nodejs_bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 8.12.0
-node -e "console.log('Running Node.js ' + process.version)"
+rm -rf /etc/yum.repos.d/nodesource-el*
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+yum install nodejs --enablerepo=nodesource
